232f1e6b866a529f5fe732b46bcfec0d6065cfd2
kendallbabbs <kendallbabbs@localhost>
2024-04-09 14:01:39 -0400

    I added a print line statement that prints "Hello World" - kendall babbs

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,6 +12,7 @@ public class Greeter {
 
   }
 
+  System.out.println("Hello World");
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
